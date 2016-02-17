9ce7f2b6e737980cee99603253c118a6603e3e86
Florian Fischer <florian.fl.fischer@fau.de>
2016-02-17 18:35:11 +0100

    change #store# layout
    Now every month has the correct amount of marks not  always 31.
    
    This change should not affect set and print_month because
    they never use a fixed assumption how many marks/days a month has.

--- functions/__mark_new_store.fish
+++ functions/__mark_new_store.fish
@@ -1,10 +1,24 @@
 function __mark_new_store --description "generates new mark store for one year"
+    set year (date "+%Y")
     echo "#colors#"
     echo "#store#"
-    echo (date "+%Y"):
+    echo $year:
     for m in (seq 1 12)
         echo -n "$m:"
-        for d in (seq 1 31)
+        switch $m
+            case 1 3 5 7 8 10 12
+                set days 31
+            case 4 6 9 11
+                set days 30
+            case 2
+                if date -d "$year-02-29" >/dev/null ^/dev/null
+                    set days 29
+                else
+                    set days 28
+                end
+        end
+
+        for d in (seq 1 $days)
             echo -n "?"
         end
         echo
